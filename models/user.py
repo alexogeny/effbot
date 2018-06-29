a749bdbf2e12adde6178613a842fd158130925f9
effrill3 <effrill3@gmail.com>
2018-06-30 00:43:14 +1000

    Slight updates to user model

--- models/user.py
+++ models/user.py
@@ -3,15 +3,15 @@ import peewee as pw
 from .base import BaseModel
 
 class UserTT2(BaseModel):
-    support_code = pw.CharField(max_length=10, index=True)
-    clan_code = pw.DeferredForeignKey('ServerTT2', null=True)
-    max_stage = pw.IntegerField(default=10000)
-    num_prestiges = pw.IntegerField(default=250)
-    num_cqs = pw.IntegerField(default=1000)
-    max_taps = pw.IntegerField(default=300,
+    supportcode = pw.CharField(max_length=10, index=True)
+    clancode = pw.DeferredForeignKey('ServerTT2', null=True)
+    ms = pw.IntegerField(default=10000)
+    prestiges = pw.IntegerField(default=250)
+    tcq = pw.IntegerField(default=1000)
+    maxtaps = pw.IntegerField(default=300,
                                constraints=[pw.Check('max_taps >= 1'),
                                             pw.Check('max_taps <= 600')])
-    lifetime_taps = pw.BigIntegerField()
+    tapslifetime = pw.BigIntegerField()
     playtime = pw.CharField(max_length=16)
-    artifact_data = pw.BlobField()
-    country_code = pw.CharField(max_length=4)
+    artifacts = pw.BlobField()
+    country = pw.CharField(max_length=4)
