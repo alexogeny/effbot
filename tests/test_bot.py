f5f655189ea58423529471136cca6ea1c3a5817b
effrill3 <effrill3@gmail.com>
2018-07-24 17:45:25 +1000

    Adjust tests file to maybe work this time? Idk

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,12 +1,12 @@
 import sqlite3
 import pytest
 import peewee
+from bot import Effbot, get_prefix
 
 
 def test_bot_exists():
     with pytest.raises(peewee.OperationalError, message="Expected no tables to exist"):
-        from bot import bot
-        assert bot
+        assert Effbot(get_prefix())
 
 # def test_bot_runs():
 #     assert bot.run()
