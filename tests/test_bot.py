7e602d486b983951c7845a81ebf6407bb518c313
effrill3 <effrill3@gmail.com>
2018-07-24 17:36:49 +1000

    Added exception on tables not existing for testing

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,7 +1,11 @@
-from bot import bot
+import sqlite3
+import pytest
+
 
 def test_bot_exists():
-    assert bot
+    with pytest.raises(sqlite3.OperationalError, message="Expected no tables to exist"):
+        from bot import bot
+        assert bot
 
 # def test_bot_runs():
 #     assert bot.run()
