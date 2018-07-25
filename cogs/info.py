cf3e60724e1da8d9946c2be489263b3b61af3c08
effrill3 <effrill3@gmail.com>
2018-07-25 23:15:41 +1000

    Reenabled user info command

--- cogs/info.py
+++ cogs/info.py
@@ -67,76 +67,74 @@ class Information():
         asyncio.ensure_future(ctx.send(commands))
 
 
-    # @info.command(name='user', pass_context=True, no_pm=True)
-    # async def _user(self, ctx, *, user: str=None):
-    #     """Shows users's informations"""
-    #     author = ctx.message.author
-    #     server = ctx.message.guild
+    @info.command(name='user', no_pm=True)
+    async def _user(self, ctx, *, user: str=None):
+        """Shows users's informations"""
+        author = ctx.author
+        server = ctx.guild
 
-    #     if not user:
-    #         user = author
-    #     elif not hasattr(user, 'roles'):
-    #         try:
-    #             user = server.get_member(int(user[2:-1]))
-    #         except:
-    #             user = [n for n in server.members
-    #                 if user.lower() in str(n.name).lower()
-    #                 or user.lower() in str(n.nick).lower()]
-    #         if isinstance(user, list) and len(user) >= 1:
-    #             user = user[0]
-    #             print(user)
-    #         elif isinstance(user, list) and len(user) == 0:
-    #             await ctx.send('I could not find that user. Try a different name or variation?')
-    #             return
-    #     roles = [x.name for x in user.roles if x.name != "@everyone"]
+        if not user:
+            user = author
+        elif not hasattr(user, 'roles'):
+            try:
+                user = server.get_member(int(user[2:-1]))
+            except:
+                user = await self.helpers.choose_member(ctx, server, user)
+        if not user:
+            return
+        roles = [x.name for x in user.roles if x.name != "@everyone"]
 
-    #     joined_at = user.joined_at
-    #     since_created = (ctx.message.created_at - user.created_at).days
-    #     since_joined = (ctx.message.created_at - user.joined_at).days
-    #     user_joined = joined_at.strftime("%d %b %Y %H:%M")
-    #     user_created = user.created_at.strftime("%d %b %Y %H:%M")
-    #     member_number = sorted(server.members,
-    #                            key=lambda m: m.joined_at).index(user) + 1
+        joined_at = user.joined_at
+        since_created = (ctx.message.created_at - user.created_at).days
+        since_joined = (ctx.message.created_at - user.joined_at).days
+        user_joined = joined_at.strftime("%d %b %Y %H:%M")
+        user_created = user.created_at.strftime("%d %b %Y %H:%M")
+        member_number = sorted(server.members,
+                               key=lambda m: m.joined_at).index(user) + 1
 
-    #     created_on = "{}\n({} days ago)".format(user_created, since_created)
-    #     joined_on = "{}\n({} days ago)".format(user_joined, since_joined)
+        created_on = "{}\n({} days ago)".format(user_created, since_created)
+        joined_on = "{}\n({} days ago)".format(user_joined, since_joined)
 
-    #     game = "Chilling in {} status".format(user.status)
+        game = "Chilling in {} status".format(user.status)
 
-    #     if user.activity is None:
-    #         pass
-    #     elif not hasattr(user.activity, 'url'):
-    #         game = "{}".format(user.activity)
-    #     else:
-    #         game = "Streaming: [{}]({})".format(user.activity, user.activity.url)
+        if user.activity is None:
+            pass
+        elif not hasattr(user.activity, 'url'):
+            game = "{}".format(user.activity)
+        else:
+            game = "Streaming: [{}]({})".format(user.activity, user.activity.url)
 
-    #     if roles:
-    #         roles = sorted(roles, key=[x.name for x in server.role_hierarchy
-    #                                    if x.name != "@everyone"].index)
-    #         roles = ", ".join(roles)
-    #     else:
-    #         roles = "None"
+        if roles:
+            # roles = sorted(roles, key=[x.name for x in server.role_hierarchy
+                                       # if x.name != "@everyone"].index)
+            roles = ", ".join([r.mention
+                               for r
+                               in server.role_hierarchy
+                               if r.name != "@everyone"
+                               and r in user.roles])
+        else:
+            roles = "None"
 
-    #     data = discord.Embed(description=game, colour=user.colour)
-    #     data.add_field(name="Joined Discord on", value=created_on)
-    #     data.add_field(name="Joined this server on", value=joined_on)
-    #     data.add_field(name="Roles", value=roles, inline=False)
-    #     data.set_footer(text="Member #{} | User ID:{}"
-    #                          "".format(member_number, user.id))
+        data = discord.Embed(description=game, colour=user.colour)
+        data.add_field(name="Joined Discord on", value=created_on)
+        data.add_field(name="Joined this server on", value=joined_on)
+        data.add_field(name="Roles", value=roles, inline=False)
+        data.set_footer(text="Member #{} | User ID:{}"
+                             "".format(member_number, user.id))
 
-    #     name = str(user)
-    #     name = " ~ ".join((name, user.nick)) if user.nick else name
+        name = str(user)
+        name = " ~ ".join((name, user.nick)) if user.nick else name
 
-    #     if user.avatar_url:
-    #         data.set_author(name=name, url=user.avatar_url)
-    #         data.set_thumbnail(url=user.avatar_url)
-    #     else:
-    #         data.set_author(name=name)
+        if user.avatar_url:
+            data.set_author(name=name, url=user.avatar_url)
+            data.set_thumbnail(url=user.avatar_url)
+        else:
+            data.set_author(name=name)
 
-    #     try:
-    #         await ctx.send(embed=data)
-    #     except discord.HTTPException:
-    #         await ctx.send("I need the `Embed links` permission to send this")
+        try:
+            await ctx.send(embed=data)
+        except discord.HTTPException:
+            await ctx.send("I need the `Embed links` permission to send this")
 
     @commands.command(pass_context=True, no_pm=True)
     async def avatar(self, ctx, user: str=None):
