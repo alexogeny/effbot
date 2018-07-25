f0618b00689ee7c20458df9ba040c8415a91ae77
effrill3 <effrill3@gmail.com>
2018-07-26 00:09:14 +1000

    Fix reporting commands

--- cogs/reporting.py
+++ cogs/reporting.py
@@ -23,47 +23,10 @@ class Reporting():
         if a.bot:
             return
         u = await self.helpers.get_record('user', m.author.id)
-        c = u['config']
         now = datetime.utcnow().timestamp()
-        c.last_bug = getattr(c, 'last_bug', 0)
-        if not c.last_bug or now - c.last_bug >= 3600:
-            c.last_bug = now
-            pfx = await self.bot.get_prefix(ctx.message)
-            mc = m.clean_content
-            passed = 0
-            while not passed:
-                for p in pfx:
-                    if mc.startswith(p):
-                        mc = mc[len(p):]
-                        passed = 1
-
-            e = await self.helpers.build_embed('Bug Report', 0xffffff)
-            e.set_thumbnail(url=a.avatar_url_as(format='jpeg'))
-            e.set_author(name=f'{a.name}#{a.discriminator}', icon_url='https://i.imgur.com/uoAST0b.png')
-            e.add_field(name='User', value=f'{a.mention} ({a.id})')
-            e.add_field(name='Content', value=mc[len(ctx.command.name):].strip())
-            asyncio.ensure_future(self.bot.get_channel(466133068730597377).send(embed=e))
-        else:
-            asyncio.ensure_future(ctx.send('You must wait at least an hour between bug reports.'))
-            return
-        asyncio.ensure_future(ctx.send(':ideograph_advantage: Thanks, you have successfully filed your bug.'))
-
-
-    @commands.command(name='suggestion', pass_context=True, no_pm=True, aliases=['suggest'])
-    async def _suggest(self, ctx):
-        m = ctx.message
-        a = m.author
-        if not len(m.content) >= 30:
-            await ctx.send('Please be more descriptive (more than 30 characters).')
-            return
-        if a.bot:
-            return
-        u = await self.helpers.get_record('user', m.author.id)
-        c = u['config']
-        now = datetime.utcnow().timestamp()
-        c.last_suggest = getattr(c, 'last_suggest', 0)
-        if now - c.last_suggest >= 3600:
-            c.last_suggest = now
+        last_bug = u.timers.get('last_bug', 0)
+        if now - last_bug >= 3600:
+            u.timers['last_bug'] = now
             pfx = await self.bot.get_prefix(ctx.message)
             mc = m.clean_content
             passed = 0
@@ -74,7 +37,42 @@ class Reporting():
                         passed = 1
             mc = re.sub(r'^\w+','',mc)
 
-            e = await self.helpers.build_embed('Suggested: {mc.strip()}', 0xffffff)
+            e = await self.helpers.build_embed(f'Bug: {mc.strip()}', 0xffffff)
+            e.set_thumbnail(url=a.avatar_url_as(format='jpeg'))
+            e.set_author(name=f'{a.name}#{a.discriminator} ({a.id})',
+                         icon_url='https://i.imgur.com/uoAST0b.png')
+            asyncio.ensure_future(self.bot.get_channel(466133068730597377).send(embed=e))
+        else:
+            asyncio.ensure_future(ctx.send('You must wait at least an hour between bug reports.'))
+            return
+        asyncio.ensure_future(ctx.send(':ideograph_advantage: Thanks, you have successfully filed your bug.'))
+
+
+    @commands.command(name='suggestion', aliases=['suggest'])
+    async def _suggest(self, ctx):
+        m = ctx.message
+        a = m.author
+        if not len(m.content) >= 30:
+            await ctx.send('Please be more descriptive (more than 30 characters).')
+            return
+        if a.bot:
+            return
+        u = await self.helpers.get_record('user', m.author.id)
+        now = datetime.utcnow().timestamp()
+        last_suggest = u.timers.get('last_suggest', 0)
+        if now - last_suggest >= 3600:
+            u.timers['last_suggest'] = now
+            pfx = await self.bot.get_prefix(ctx.message)
+            mc = m.clean_content
+            passed = 0
+            while not passed:
+                for p in pfx:
+                    if mc.startswith(p):
+                        mc = mc[len(p):]
+                        passed = 1
+            mc = re.sub(r'^\w+','',mc)
+
+            e = await self.helpers.build_embed(f'Suggested: {mc.strip()}', 0xffffff)
             e.set_thumbnail(url=a.avatar_url_as(format='jpeg'))
             e.set_author(name=f'{a.name}#{a.discriminator} ({a.id})',
                            icon_url='https://i.imgur.com/6y7oNyd.png')
