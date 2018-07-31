f5186cbd60961b49b373c2103c2b905f20b70e96
effrill3 <effrill3@gmail.com>
2018-07-31 18:24:18 +1000

    Fixed status rotator and added avatar rotator

--- cogs/status.py
+++ cogs/status.py
@@ -1,81 +1,96 @@
-import os, time
+# import os, time
 import discord
+from discord.http import Route
 import asyncio
+from datetime import datetime
 from discord.ext import commands
+import base64
 from random import choice as rndchoice
+from pathlib import Path
 
 ACTIVITY_TYPES = ['playing', 'streaming', 'listening', 'watching']
 
-def is_owner():
-    async def predicate(ctx):
-        return ctx.author.id == 305879281580638228
-    return commands.check(predicate)
+# def is_owner():
+    # async def predicate(ctx):
+        # return ctx.author.id == 305879281580638228
+    # return commands.check(predicate)
 
 class RandomStatus():
     """docstring for RandomStatus"""
     def __init__(self, bot):
         
         self.bot = bot
-        self.last_change = None
-        self.delay = 300
-        self.statuses = ['playing in {} guilds',
-                         'watching over {} users',
-                         'listening the sounds of nature',
-                         'listening the wind',
-                         'listening the deep hum of mystical forces',
-                         'playing in the fields of yonder',
-                         'playing the one single source of truth CD',
-                         'playing with time']
+        self._cwd = Path('..')
+        self.last_status_change = None
+        self.last_avatar_change = None
+        self.last_avatar_name = None
+        self.delay = 600
+        self.statuses = [
+            ('playing', 'in {guilds} guilds'),
+            ('watching', 'over {users} users'),
+            ('listening', 'the wind'),
+            ('listening', 'the deep hum of mystical forces'),
+            ('watching', 'footprints on the sands of time'),
+            ('watching', 'grass grow'),
+            ('watching', 'paint dry'),
+            ('playing', 'God'),
+            ('playing', 'the race card'),
+            ('playing', 'with a full deck'),
+            ('playing', 'with a loaded gun'),
+            ('playing', 'Russian roulette'),
+            ('playing', 'in the big leagues'),
+            ('playing', 'effrill3 for a fool')
+        ]
+        self.avatars = {
+            p.name: 'data:image/png;base64,'+base64.b64encode(p.open('rb').read()).decode('utf-8')
+            for p
+            in self._cwd.rglob('avatar ([1-9]).jpg')}
+        # print(self.statuses)
+    def edit_profile(self, avatar):
+        payload = {
+            'username': 'effbot',
+            'avatar': avatar
+        }
+        return self.bot.http.request(Route('PATCH', '/users/@me'), json=payload)
 
-    @commands.group(pass_context=True, invoke_without_command=True)
-    @is_owner()
-    async def status(self, ctx):
-        await ctx.send("Um, you need to tell me what to do ...")
+    @commands.guild_only()
+    async def rotate_avatar(self, m):
+        now = datetime.utcnow()
+        if not self.last_avatar_change or (now-self.last_avatar_change).total_seconds() >= self.delay*6:
+            self.last_avatar_change = now
+            self.last_avatar_name = rndchoice([p for p in self.avatars if self.last_avatar_name != p])
+            # print(self.avatars[self.last_avatar_name])
+            asyncio.ensure_future(self.edit_profile(avatar=self.avatars[self.last_avatar_name]))
 
-    @status.command(name='cycle', pass_context=True, no_pm=True)
-    async def _set(self, ctx):
-        current_status = ctx.message.guild.me.activity
-        self.last_change = int(time.perf_counter())
-        new_game, activity_type = self.random_status(ctx.message)
-        await self.bot.change_presence(
-           activity=(discord.Activity(name=new_game, type=activity_type)), status=current_status)
-        await ctx.send("Ok, roger that, I've cycled my status. Nerd!")
+    @commands.guild_only()
+    async def rotate_status(self, m):
+        me, now = m.guild.me, datetime.utcnow()
+        activity, presence = me.activity, me.status
+        # print(activity)
+        # print(presence)
+        if not self.last_status_change or (now - self.last_status_change).total_seconds() >= self.delay:
+            self.last_status_change = now
+            status, activity = await self.choose_status(activity)
+            # print(status)
+            # print(activity)
+            asyncio.ensure_future(self.change_status(status, activity, presence))
+    
+    async def change_status(self, status, activity, presence):
+        # print('changing status')
+        act = discord.Activity(
+            name=activity.format(guilds=len(self.bot.guilds), users=len(self.bot.users)),
+            type='playing streaming listening watching'.split().index(status)
+        )
+        await self.bot.change_presence(activity=act, status=presence)
+    
+    async def choose_status(self, current):
+        # while current == new:
+        status, activity = rndchoice([(x,y) for x,y in self.statuses if y != current])
+        return status, activity
 
-    async def switch_status(self, message):
-        if not isinstance(message.channel, discord.abc.PrivateChannel):
-            current_game = str(message.guild.me.activity)
-            current_status = message.guild.me.status
-
-            if self.last_change == None: #first run
-                self.last_change = int(time.time())
-                if len(self.statuses) > 0 and (current_game in self.statuses or current_game == "None"):
-                    new_game, activity_type = self.random_status(message)
-                    await self.bot.change_presence(
-                       activity=(discord.Activity(name=new_game, type=activity_type)), status=current_status)
-
-            if message.author.id != self.bot.user.id:
-                if abs(self.last_change - int(time.time())) >= self.delay:
-                    self.last_change = int(time.time())
-                    new_game, activity_type = self.random_status(message)
-                    if new_game != None:
-                        if current_game != new_game:
-                            if current_game in self.statuses or current_game == "None":
-                                await self.bot.change_presence(
-                                    activity=(discord.Activity(name=new_game, type=activity_type)), status=current_status)
-    def random_status(self, msg):
-        current = str(msg.guild.me.activity)
-        new = str(msg.guild.me.activity)
-        if len(self.statuses) > 1:
-            while current == new:
-                new = rndchoice(self.statuses)
-                if '{}' in new:
-                    new = new.format(len(getattr(self.bot, new.split(' ')[-1])))
-                
-        else:
-            new = None
-        return ' '.join(new.split(' ')[1:]), ACTIVITY_TYPES.index(new.split(' ')[0])
-
+        
 def setup(bot):
     cog = RandomStatus(bot)
-    bot.add_listener(cog.switch_status, "on_message")
+    bot.add_listener(cog.rotate_status, "on_message")
+    bot.add_listener(cog.rotate_avatar, "on_message")
     bot.add_cog(cog)
